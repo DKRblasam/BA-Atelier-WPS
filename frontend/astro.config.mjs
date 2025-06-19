35052bb1b10ed2b08e1981000d8d2e6fd3bc0d06
DRKatan <fer.mtz.guz@gmail.com>
2025-06-18 19:36:45 -0600

    Remove unused components and scripts from the frontend, including Header, Hero, Services, Gallery, Footer, and Layout. Delete associated styles and TypeScript configuration. Clean up the project structure by eliminating unnecessary files and references.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,35 @@
 // @ts-check
+// https://astro.build/config
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
+
+
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+import { neobrutalism } from "@clerk/themes";
+import { esMX } from '@clerk/localizations'
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [
+    clerk({
+      appearance: {
+        captcha: {
+          theme: "dark",
+          size: "flexible",
+          language: "es-MX",
+        },
+        baseTheme: neobrutalism,
+      },
+      localization: esMX,
+    }),
+  ],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,8 +1,6 @@
 // tailwind.config.mjs
 import animations from "@midudev/tailwind-animations";
 
-
-
 export default {
   // rest of the options
   plugins: [animations],
